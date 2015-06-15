95a838a0ee17f08bcfce7dc725c55339340903e5
2015CLanguage <buaa_2015_c@163.com>
2015-06-15 21:27:52 +0800

    Create 14151200_陈维正_9_2.c

---
+++ 14151200_陈维正_9_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+int main()
+{
+int days(int x,int y,int z);
+int a,b,c;
+printf("请输入年月日：");
+scanf("%d%d%d",&a,&b,&c);
+printf("该天是本年的第%d天",days(a,b,c));
+}
+
+int days(int x,int y,int z)
+{
+int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+if(z%100!=0&&z%4==0)
+	a[1]=29;
+if(z%400==0)
+	a[1]=29;
+int i,h=0;
+for(i=0;i<=y-2;i++)
+	h=h+a[i];
+h=h+z;
+return(h);
+}

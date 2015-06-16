3756064f570de6290b5260d6fb75e9aa2da7443f
2015CLanguage <buaa_2015_c@163.com>
2015-06-16 20:30:38 +0800

    Update 14151203 _李永豪_10_7.c

--- 14151203 _李永豪_10_7.c
+++ 14151203 _李永豪_10_7.c
@@ -45,6 +45,7 @@ int main(int argc, const char * argv[]) {
             break;
         }
     }
+    fclose(fp);
     //write in file stu_add
     printf("\nstu_add:\n");
     fp=fopen("stu_add.dat","w");
@@ -67,5 +68,6 @@ int main(int argc, const char * argv[]) {
             printf("%5.2f",stu[i].grade[j]);
         printf("%5.2f",stu[i].ave);
     }
+     fclose(fp);
     return 0;
 }

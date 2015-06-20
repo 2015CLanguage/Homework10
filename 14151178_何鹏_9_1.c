f1a39135f88e47a227d7f2ac89c4d0a18bfb8210
2015CLanguage <buaa_2015_c@163.com>
2015-06-20 19:29:45 +0800

    Create 14151178_何鹏_9_1.c

---
+++ 14151178_何鹏_9_1.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+struct date
+{
+	int year;
+	int month;
+	int day;
+}d;
+int cal(struct date *p);
+int main()
+{
+	int num;
+	struct date *p=&d;
+	printf("请依次输入年月日：");
+	scanf("%d%d%D",&d.year,&d.month,&d.day);
+	num=cal(p);
+	printf("%d",num);
+}
+
+int cal(struct date *p)
+{
+	int num=0;
+	int i,k;
+	if(p->year%4!=0||(p->year%100==0&&p->year%400!=0))
+	    k=0;
+	else
+	    k=1;
+	int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	for(i=0;i<(p->month);i++)
+	    num=num+a[i];
+	if(k==0||(k=1&&(p->month)>2))
+	    num++;
+	return num;
+}

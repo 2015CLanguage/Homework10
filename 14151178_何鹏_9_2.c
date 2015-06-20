48db127e86daf7b57a5eca4d122743f2c2127574
2015CLanguage <buaa_2015_c@163.com>
2015-06-20 19:40:51 +0800

    Create 14151178_何鹏_9_2.c

---
+++ 14151178_何鹏_9_2.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+struct date
+{
+	int year;
+	int month;
+	int day;
+}d;
+int days(int year,int month,int day);
+int main()
+{
+	int num;
+	printf("请依次输入年月日：");
+	scanf("%d%d%D",&d.year,&d.month,&d.day);
+	num=days(d.year,d.month,d.day);
+	printf("%d",num);
+}
+
+int days(int year,int month,int day)
+{
+	int num=0;
+	int i,k;
+	if(year%4!=0||(year%100==0&&year%400!=0))
+	    k=0;
+	else
+	    k=1;
+	int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	for(i=0;i<month;i++)
+	    num=num+a[i];
+	if(k==1&&month>2)
+	    num++;
+	return num;
+}

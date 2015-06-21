a228a4c061d5f0ac0b5d7e79a24d1a28814b22e4
2015CLanguage <buaa_2015_c@163.com>
2015-06-21 11:12:11 +0800

    Update 14151152_糟一凡_10_6.c

--- 14151152_糟一凡_10_6.c
+++ 14151152_糟一凡_10_6.c
@@ -7,11 +7,11 @@ struct Student
 	float score[3];
 	float aver;
 };
-struct Student stu[N];
+struct Student stu[N],temp;
 void save();
 int main()
 {
-	int i;
+	int i,j;
 	printf("请输入5个学生的学号，姓名，三门课的成绩：\n"); 
 	for(i=0;i<N;i++)
 	{
@@ -19,6 +19,41 @@ int main()
 		stu[i].aver=(stu[i].score[0]+stu[i].score[1]+stu[i].score[2])/3.0; 
 	}
 	save();
+	FILE *fp1;
+	fp1=fopen("stud.dat","rb");
+	for(i=0;i<N;i++)
+	{
+	    fread(&stu[i],sizeof(struct Student),1,fp1);
+    }
+    fclose(fp1);
+    for (i=0; i<N; i++)
+    {
+	    for (j=i+1; j<N; j++)
+	    {
+	        if (stu[i].aver<stu[j].aver)
+	        {
+	  	        temp=stu[i];
+				stu[i]=stu[j];
+				stu[j]=temp;
+	  	    }
+	    }
+    }
+    FILE *fp2;
+    if((fp2=fopen("stu_sort.dat","wb"))==NULL)
+	{
+		printf("文件无法打开！\n");
+		return 0;
+	}
+	printf("排序后：\n");
+	for(i=0;i<N;i++)
+	{
+		if(fwrite(&stu[i],sizeof(struct Student),1,fp2)!=1)
+		{
+			printf("数据写入错误！"); 
+		}
+		printf("学号：%d 姓名：%s 三门课成绩：%5.1f %5.1f %5.1f 平均分数：%5.1f",stu[i].index,stu[i].name,stu[i].score[0],stu[i].score[1],stu[i].score[2],stu[i].aver);
+	}
+	fclose(fp2);
 	return 0;
 }
 void save()
@@ -38,3 +73,5 @@ void save()
 	}
 	fclose(fp);
 }
+
+

7335461a6ec819f207493a489de5f6eaeb0deba8
2015CLanguage <buaa_2015_c@163.com>
2015-06-21 13:28:35 +0800

    Update 14151200_陈维正_10_6.c

--- 14151200_陈维正_10_6.c
+++ 14151200_陈维正_10_6.c
@@ -7,12 +7,11 @@ char num[10];
 char name[10];
 float score[3]={0};
 float avep;
-}stu[N];
+}stu[N],temp;
 
 int main()
 {
 int i,j;
-float temp;
 FILE*fp,*in;
 if((fp=fopen("stu.dat","r"))==NULL)
 	{
@@ -27,13 +26,14 @@ for(i=0;i<N;i++)
 		printf("%8.2f",stu[i].score[j]);
 	printf("%8.2f\n",stu[i].avep);
 }
+fclose(fp);
 for(i=0;i<N;i++)
 	{
 	if(stu[i].avep<stu[i+1].avep)
 		{
-		temp=stu[i+1].avep;
-		stu[i+1].avep=stu[i].avep;
-		stu[i].avep=temp;
+		temp=stu[i+1];
+		stu[i+1]=stu[i];
+		stu[i]=temp;
 		}
 	}
 in=fopen("stu_sort.dat","w");
@@ -44,12 +44,11 @@ for(i=0;i<N;i++)
 	}
 for(i=0;i<N;i++)
 	{
-	printf("%s%s",stu[i].num,stu[i].name);
+	printf("%s %s",stu[i].num,stu[i].name);
 	for(j=0;j<3;j++)
 		{printf("%8.2f",stu[i].score[j]);}
 	printf("%8.2f\n",stu[i].avep);
 	}
-fclose(fp);
 fclose(in);
 return 0;
 }

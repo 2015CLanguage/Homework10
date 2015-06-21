42a3f446dd414da847ac6ff5a5b8cc491d4238af
2015CLanguage <buaa_2015_c@163.com>
2015-06-22 01:06:47 +0800

    Create 14151192_冯浩_9_5.c

---
+++ 14151192_冯浩_9_5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+struct student
+{
+	int Id;
+	double grade[3];
+	double aver;
+	char Name[10];
+}s[10];
+int main()
+{
+	int i,j;
+	for (i=0; i<10; i++)
+	{
+		scanf("%d,%s",&s[i].Id,&s[i].Name);
+		for (j=0;j<3;j++)
+			scanf("%lf",&s[i].grade[j]);	
+		s[i].aver=(s[i].grade[0]+s[i].grade[1]+s[i].grade[2])/3;
+	}
+	double MaxGrade=0;
+	int id;
+	for (i=0; i<10; i++)
+	{
+		if (s[i].aver>MaxGrade)
+		{
+			MaxGrade=s[i].aver;
+			id=i;
+		}
+	}
+	for (i=0; i<10; i++)	
+		printf("%5.2f\n",s[i].aver);
+	printf("最高分为:");
+	printf("%d %s ",s[id].Id,s[id].Name);
+	for (j=0; j<3; j++)  
+		printf("%5.2lf ",s[id].grade[j]);
+	printf("%5.2lf\n",s[id].aver);
+	return 0;
+}

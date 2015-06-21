22e184bae11b4fd3b26b61810f5bfae7d67b5e11
2015CLanguage <buaa_2015_c@163.com>
2015-06-21 08:51:48 +0800

    Create 14151124_孙宇童_9.1.c

---
+++ 14151124_孙宇童_9.1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int sum_day(int month,int day);
+int leap(int year);
+int main(void)
+{	int day_tab[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+		int year,month,day,i,days=0;
+		struct Date
+	{
+		int year;
+		int month;
+		int day;
+	}date;
+
+	printf("请输入日期：\n");
+	scanf("%d%d%d",&date.year,&date.month,&date.day);
+	
+	
+		for(i=1;i<date.month;i++)
+		{
+			days=days+day_tab[i];
+			days=days+date.day;}
+		if((date.year%4==0&&date.year%100!=0||date.year%400==0)&&month>=3)
+			days+=1;
+	
+	printf("这一天是该年的第%d天",days);
+
+	return 0;
+	}

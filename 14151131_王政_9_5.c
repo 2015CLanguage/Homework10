dc7f4d9277772a65e04d1a0088d9753e70560467
2015CLanguage <buaa_2015_c@163.com>
2015-06-22 20:06:16 +0800

    Create 14151131_王政_9_5.c

---
+++ 14151131_王政_9_5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+struct Students
+{
+	char name;
+	int number;
+	int score[3];
+};
+int main()
+{
+	struct Students test[10];
+	int i,j,every_sum=0,score_sum=0,max=0,sum[10];
+	printf("请依次输入每名学生的姓名、学号、各科成绩\n");
+	for(i=0;i<10;i++)
+	{
+		scanf("%s",&test[i].name);
+		scanf("%d",&test[i].number);
+		for(j=0;j<3;j++)
+		{
+		scanf("%d",&test[i].score[j]);
+     	}
+	}
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			every_sum=every_sum+test[i].score[j];
+		}
+		sum[i]=every_sum;
+	}
+	for(i=0;i<10;i++)
+	{
+		score_sum=score_sum+sum[i];
+	}
+	printf("平均分为%d\n",score_sum/10);
+	for(i=0;i<10;i++)
+	{
+		if(sum[max]<sum[i])
+		{
+			max=i;
+		}
+	}
+	printf("最高分学生为%s，学号为%d，成绩为%d\n",test[max].name,test[max].number,sum[max]);
+	return 0;
+}

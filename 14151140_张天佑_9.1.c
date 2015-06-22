1ecaeb0596c0322eaa23ecaca24356de8b8b64ab
2015CLanguage <buaa_2015_c@163.com>
2015-06-22 16:27:56 +0800

    Create 14151140_张天佑_9.1.c

---
+++ 14151140_张天佑_9.1.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+struct Date
+{
+	int year;
+	int month;
+	int day;
+}date;
+int cal(struct Date date)
+{
+	int tab1[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }, tab2[13] = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int i;
+	if ((date.year % 4 == 0 && date.year % 100 != 0) || date.year % 400 == 0)           //判断是否为闰年
+	{
+		for (i = 1; i < date.month; i++)                                  //累加所在月之前天数
+		{
+			date.day += tab2[i];
+		}
+	}
+	else
+	{
+		for (i = 1; i < date.month; i++)
+		{
+			date.day += tab1[i];
+		}
+	}
+	return(date.day);
+}
+void main()
+{
+	int days;
+	printf("输入日期:\n");
+	scanf_s("%d.%d.%d", &date.year, &date.month, &date.day);
+	days = cal(date);                             //调用函数输出
+	printf("%d年%d月%d日是那一年的第%d天.\n", date.year, date.month, date.day, days);
+}

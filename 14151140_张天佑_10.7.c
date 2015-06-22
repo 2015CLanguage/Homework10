f52fe56ecbdd4228a311294590acca885afe0484
2015CLanguage <buaa_2015_c@163.com>
2015-06-22 20:50:20 +0800

    Update 14151140_张天佑_10.7.c

--- 14151140_张天佑_10.7.c
+++ 14151140_张天佑_10.7.c
@@ -23,47 +23,19 @@ void main()
 		printf("can not open.\n");
 		exit(0);
 	}
-	printf("文件 stu_sort:\n");
 	for (i = 0; fread(&stu[i], sizeof(struct Student), 1, fp) != 0; i++)
-	{
-		printf("\n%8d%8s", stu[i].num, stu[i].name);
-		for (j = 0; j < 3; j++)
-		{
-			printf("%8d", stu[i].score[j]);
-		}
-		printf("%10.2f", stu[i].ave);
-	}
 	printf("\n");
 	n = i;                                       
 	for (b = 0; stu[b].ave>a.ave&&b < n; b++)
-	printf("插入后:\n");
 	fp1 = fopen("sort1.dat", "w");                             //输出排序结果
 	for (i = 0;i<b; i++)
 	{
 		fwrite(&stu[i], sizeof(struct Student), 1, fp1);
-		printf("\n%8d%8s", stu[i].num, stu[i].name);
-		for (j = 0; j < 3; j++)
-		{
-			printf("%8d", stu[i].score[j]);
-		}
-		printf("%10.2f", stu[i].ave);
 	}
 	fwrite(&a, sizeof(struct Student), 1, fp1);              //实现插入
-	printf("\n%8d%8s", a.num, a.name);
-	for (j = 0; j < 3; j++)
-	{
-		printf("%8d", a.score[j]);
-	}
-	printf("%10.2f", a.ave);
 	for (i = b; i<n; i++)
 	{
 		fwrite(&stu[i], sizeof(struct Student), 1, fp1);
-		printf("\n%8d%8s", stu[i].num, stu[i].name);
-		for (j = 0; j < 3; j++)
-		{
-			printf("%8d", stu[i].score[j]);
-		}
-		printf("%10.2f", stu[i].ave);
 	}
 	printf("\n");
 	fclose(fp);

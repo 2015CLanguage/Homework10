33a9ea6e2ed949a93dc482604ab5ea3d7c1011c5
2015CLanguage <buaa_2015_c@163.com>
2015-06-22 20:05:38 +0800

    Create 14151131_王政_9_2.c

---
+++ 14151131_王政_9_2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int days(int x,int y,int z);
+struct Date
+{
+	int year;
+	int month;
+	int day;
+};
+struct Date date;
+int days(int x,int y,int z)
+{
+	int i,s;
+	int m[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	if(y==1)
+	{
+		return z;
+	}
+	else if(y==2)
+	{
+		return 31+z;
+	}
+	else
+	{
+		s=0;
+		for(i=0;i<y-1;i++)
+		{
+			s=s+m[i];
+		}
+		if(x%400==0||(x%4==0&&x%100!=0))
+		{
+			return s+z+1;
+		}
+		else
+		{
+			return s+z;
+		}
+	}
+}
+int main()
+{
+	printf("请输入具体年月日：");
+	scanf("%d.%d.%d",&date.year,&date.month,&date.day);
+	printf("该日是该年的第%d天",days(date.year,date.month,date.day));
+	return 0;
+}
